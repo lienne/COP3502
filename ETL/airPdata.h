9a9c0655d07e444ac4635e207e63b8f10dbd3b44
lienne <lianasevero3@gmail.com>
2017-06-28 01:56:44 -0400

    Added header file airPdata

---
+++ ETL/airPdata.h
@@ -0,0 +1,10 @@
+typedef struct airPdata{
+	char*siteNumber; //FAA Site Number
+	char*LocID;   //Airport’s ‘‘Short Name’’, \textit{e.g.} MCO
+	char*fieldName; //Airport Name
+	char*city;    //Associated City
+	char*state;   //State
+	char*latitude; //Latitude
+	char*longitude; //Longitude
+	char controlTower;//Control Tower, this is a single character (Y/N)
+} airPdata;
